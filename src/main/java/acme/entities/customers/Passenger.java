fb6ca59143fec0db82b504cfbddc4a9e30cdca41
naredo <nachonaredobernardos@gmail.com>
2025-02-25 11:49:14 +0100

    passenger creado

--- src/main/java/acme/entities/customers/Passenger.java
+++ src/main/java/acme/entities/customers/Passenger.java
@@ -1,6 +1,63 @@
 
 package acme.entities.customers;
 
-public class Passenger {
+import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidEmail;
+import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidString;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Passenger extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	//Atributos -----------------------------------
+	@Mandatory
+	@ValidString(max = 256)
+	@Automapped
+	private String				fullName;
+
+	@Mandatory
+	@ValidEmail
+	@Automapped
+	private String				email;
+
+	@Mandatory
+	@ValidString(pattern = "^[A-Z0-9]{6,9}$")
+	@Automapped
+	private String				passportNumber;
+
+	@Mandatory
+	@ValidMoment(past = true)
+	@Temporal(TemporalType.TIMESTAMP)
+	@Automapped
+	private Date				dateBirth;
+
+	@Optional
+	@ValidString(min = 51)
+	@Automapped
+	private String				specialNeeds;
+
+	//Relationsships
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false) //No se si es true
+	@Automapped
+	private Booking				booking;
 }
