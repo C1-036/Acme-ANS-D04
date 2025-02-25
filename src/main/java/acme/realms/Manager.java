9304c4edea69fa7fa9b52941fc216656d5894424
alesevbar <alexuscrack@gmail.com>
2025-02-25 11:34:12 +0100

    Entidad arreglada tras primera revisión

--- src/main/java/acme/realms/Manager.java
+++ src/main/java/acme/realms/Manager.java
@@ -15,22 +15,20 @@ package acme.realms;
 import java.util.Date;
 
 import javax.persistence.Column;
-import javax.persistence.Embedded;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import javax.validation.constraints.Pattern;
 
 import org.springframework.data.annotation.Transient;
 
 import acme.client.components.basis.AbstractRole;
+import acme.client.components.mappings.Automapped;
 import acme.client.components.principals.DefaultUserIdentity;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
 import acme.client.components.validation.ValidUrl;
-import acme.datatypes.UserIdentity;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -44,23 +42,25 @@ public class Manager extends AbstractRole {
 	private static final long	serialVersionUID	= 1L;
 
 	// Attributes -------------------------------------------------------------
-
-	@Column(unique = true)
-	@Pattern(regexp = "^[A-Z]{2,3}\\d{6}$", message = "{validation.realms.manager.identifierNumber}")
 	@Mandatory
-
+	@ValidString(pattern = "^[A-Z]{2,3}\\d{6}$")
+	@Column(unique = true)
+	@Automapped
 	private String				identifierNumber;
 
 	@Mandatory
 	@ValidNumber(min = 0, max = 100, integer = 3, fraction = 0)
+	@Automapped
 	private Integer				yearsOfExperience;
 
 	@Mandatory
 	@Temporal(TemporalType.DATE)
+	@Automapped
 	private Date				dateOfBirth;
 
-	@ValidUrl
 	@Optional
+	@ValidUrl
+	@Automapped
 	private String				picture;
 	// Derived attributes -----------------------------------------------------
 
@@ -94,10 +94,5 @@ public class Manager extends AbstractRole {
 	}
 
 	// Relationships ----------------------------------------------------------
-
-
-	@Embedded
-	@JoinColumn(name = "user_identity_id")
-	private UserIdentity userIdentity;
 
 }
