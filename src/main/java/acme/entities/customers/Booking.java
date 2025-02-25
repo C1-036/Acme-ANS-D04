cbadef74f3531daabc20cba46dcc4091f132a740
naredo <nachonaredobernardos@gmail.com>
2025-02-25 11:25:29 +0100

    correcciones sesion follow up

--- src/main/java/acme/entities/customers/Booking.java
+++ src/main/java/acme/entities/customers/Booking.java
@@ -3,6 +3,7 @@ package acme.entities.customers;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
@@ -11,12 +12,14 @@ import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
 import acme.client.components.datatypes.Money;
+import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidCreditCard;
 import acme.client.components.validation.ValidMoment;
 import acme.client.components.validation.ValidMoney;
 import acme.client.components.validation.ValidString;
+import acme.realms.Customers;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -31,28 +34,36 @@ public class Booking extends AbstractEntity {
 
 	@Mandatory
 	@ValidString(pattern = "^[A-Z0-9]{6,8}$")
+	@Column(unique = true)
+	@Automapped
 	//No se como hacer que sean unicas
 	private String				locatorCode;
 
 	@Mandatory
 	@ValidMoment(past = true)
 	@Temporal(TemporalType.TIMESTAMP)
+	@Automapped
 	private Date				purchaseMoment;
 
 	@Mandatory
+	@Valid
+	@Automapped
 	private TravelClass			travelClass;
 
 	@Mandatory
 	@ValidMoney
+	@Automapped
 	private Money				price;
 
 	@Optional
 	@ValidCreditCard
-	private String				creditCart; //Es solo el ultimo par de numeros, ¿como hago eso?
+	@Automapped
+	private String				creditCart; //ATRIBUTO CUSTOM TAMBIEN TENGO QUE HACERLO EL VIERNES 
 
 	// Relationships ----------------------------------------------------------
 	@Mandatory
 	@Valid
 	@ManyToOne(optional = false)
+	@Automapped
 	private Customers			customers;  //No estoy seguro
 }
