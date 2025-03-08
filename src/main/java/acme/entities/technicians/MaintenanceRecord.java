82d8580627170aeae45339f79c2c00e44ab73208
DavidEscuderoAldana <davescald@alum.us.es>
2025-03-08 10:26:03 +0100

    Prueba

--- src/main/java/acme/entities/technicians/MaintenanceRecord.java
+++ src/main/java/acme/entities/technicians/MaintenanceRecord.java
@@ -4,24 +4,20 @@ package acme.entities.technicians;
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
-import javax.validation.constraints.Min;
 
 import acme.client.components.basis.AbstractEntity;
+import acme.client.components.datatypes.Money;
 import acme.client.components.mappings.Automapped;
 import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidMoment;
+import acme.client.components.validation.ValidMoney;
 import acme.client.components.validation.ValidString;
-import acme.realms.Technician;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -36,45 +32,37 @@ public class MaintenanceRecord extends AbstractEntity {
 	@ValidMoment(past = true)
 	@Temporal(TemporalType.TIMESTAMP)
 	@Automapped
-	@Column(nullable = false)
 	private Date				moment;
 
 	@Mandatory
+	@Valid
 	@Automapped
-	@Enumerated(EnumType.STRING)
-	@Column(nullable = false)
 	private Status				status;
 
 	@Mandatory
 	@ValidMoment
 	@Temporal(TemporalType.DATE)
 	@Automapped
-	@Column(nullable = false)
 	private Date				nextInspection;
 
 	@Mandatory
-	@Min(0)
+	@ValidMoney(min = 0)
 	@Automapped
-	@Column(nullable = false)
-	private double				estimatedCost;
+	private Money				estimatedCost;
 
 	@Optional
 	@ValidString(max = 255)
 	@Automapped
 	private String				notes;
 
-	@Mandatory
-	@Valid
-	@ManyToOne
-	@Automapped
-	private Technician			technician;
+	//@Mandatory
+	//@Valid
+	//@ManyToOne
+	//@Automapped
+	//private Technician			technician;
 
 	@Valid
 	@OneToMany
 	private List<Task>			tasks;
 
-
-	public enum Status {
-		PENDING, IN_PROGRESS, COMPLETED
-	}
 }
