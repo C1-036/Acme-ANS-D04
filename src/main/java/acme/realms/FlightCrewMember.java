352a21dd40b15e211f057f791060ed4267963cf0
Adrián Robles Borrego <adrrobbor@alum.us.es>
2025-03-09 18:38:09 +0100

    FlightCrewMembers Entity

---
+++ src/main/java/acme/realms/FlightCrewMember.java
@@ -0,0 +1,69 @@
+
+package acme.realms;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+import acme.client.components.basis.AbstractRole;
+import acme.client.components.mappings.Automapped;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.Optional;
+import acme.client.components.validation.ValidNumber;
+import acme.client.components.validation.ValidString;
+import acme.entities.flightCrewMembers.AvailabilityStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class FlightCrewMember extends AbstractRole {
+
+	// Serialisation version --------------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Mandatory
+	@Column(unique = true)
+	@ValidString(pattern = "^[A-Z]{2,3}\\d{6}$")
+	private String				employeeCode;
+
+	@Automapped
+	@Mandatory
+	@ValidString(pattern = "^\\+?\\d{6,15}$")
+	private String				phoneNumber;
+
+	@Automapped
+	@Mandatory
+	@ValidString(max = 255)
+	private String				languageSkills;
+
+	@Automapped
+	@Mandatory
+	@Enumerated(EnumType.STRING)
+	private AvailabilityStatus	availabilityStatus;
+
+	@Automapped
+	@Mandatory
+	@ValidString
+	private String				airline;
+
+	@Automapped
+	@Mandatory
+	@ValidNumber(min = 0)
+	private double				salary;
+
+	@Automapped
+	@Optional
+	@ValidNumber(min = 0)
+	private Integer				yearsOfExperience;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+}
