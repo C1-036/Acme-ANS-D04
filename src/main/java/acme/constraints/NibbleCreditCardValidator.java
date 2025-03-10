1924bc74162e29f1dcd30fd742a1f3759c8b294a
naredo <nachonaredobernardos@gmail.com>
2025-03-10 10:06:38 +0100

    validacion personalizada tarjeta

---
+++ src/main/java/acme/constraints/NibbleCreditCardValidator.java
@@ -0,0 +1,46 @@
+
+package acme.constraints;
+
+import javax.validation.ConstraintValidatorContext;
+
+import acme.client.components.validation.AbstractValidator;
+import acme.client.components.validation.Validator;
+import acme.entities.customers.Booking;
+import acme.entities.customers.BookingRepository;
+
+@Validator
+public class NibbleCreditCardValidator extends AbstractValidator<ValidNibbleCreditCard, String> {
+
+	private BookingRepository	repository;
+	private Booking				booking;
+
+	//ConstraintValidator interface -------------------------------------------
+
+
+	@Override
+	protected void initialise(final ValidNibbleCreditCard annotation) {
+		assert annotation != null;
+	}
+
+	@Override
+	public boolean isValid(final String value, final ConstraintValidatorContext context) {
+		assert context != null;
+
+		boolean result;
+		boolean isNull;
+
+		isNull = this.booking == null || this.booking.getCreditCard() == null;
+
+		if (!isNull) {
+			String codeCreditCard;
+			boolean cuatroDigitos;
+
+			codeCreditCard = this.repository.findCreditCardByBookingId(this.booking.getId());
+			cuatroDigitos = value.equals(codeCreditCard) || codeCreditCard.length() == 4;
+			super.state(context, cuatroDigitos, "Cuatro digitos", "acme.validaton.booking.nibble.message");
+		}
+		result = !super.hasErrors(context);
+
+		return result;
+	}
+}
