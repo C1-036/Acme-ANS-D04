47a8cbc8eb904eda4599e720cd76d910141601ff
naredo <nachonaredobernardos@gmail.com>
2025-03-21 09:25:43 +0100

    arreglo medio

--- src/main/java/acme/features/customer/passenger/CustomerPassengerRepository.java
+++ src/main/java/acme/features/customer/passenger/CustomerPassengerRepository.java
@@ -15,7 +15,7 @@ public interface CustomerPassengerRepository extends AbstractRepository {
 	@Query("SELECT m.passenger FROM Make m WHERE m.booking.customer.id = :id")
 	Collection<Passenger> findAllPassengerByBooking(int id);
 
-	@Query("select p from Passenger p where p.id = :id")
-	Passenger findPassengerById(int id);
+	@Query("SELECT m.passenger FROM Make m WHERE m.booking.id = :bookingId")
+	Passenger findPassengerByBookingId(int bookingId);
 
 }
--- src/main/java/acme/features/customer/passenger/CustomerPassengerShowService.java
+++ src/main/java/acme/features/customer/passenger/CustomerPassengerShowService.java
@@ -3,6 +3,7 @@ package acme.features.customer.passenger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import acme.client.components.models.Dataset;
 import acme.client.services.AbstractGuiService;
 import acme.client.services.GuiService;
 import acme.entities.customers.Passenger;
@@ -18,16 +19,37 @@ public class CustomerPassengerShowService extends AbstractGuiService<Customer, P
 	@Override
 	public void authorise() {
 		boolean status;
-		int id;
+		int bookingId;
 		Passenger passenger;
 
-		id = super.getRequest().getData("id", int.class);
-		passenger = this.repository.findPassengerById(id);
+		bookingId = super.getRequest().getData("id", int.class);
+		passenger = this.repository.findPassengerByBookingId(bookingId);
 
 		status = passenger != null;
 
 		super.getResponse().setAuthorised(status);
 
+	}
+
+	@Override
+	public void load() {
+		Passenger passenger;
+		int id;
+
+		id = super.getRequest().getData("id", int.class);
+		passenger = this.repository.findPassengerByBookingId(id);
+
+		super.getBuffer().addData(passenger);
+
+	}
+
+	@Override
+	public void unbind(final Passenger passenger) {
+		Dataset dataset;
+
+		dataset = super.unbindObject(passenger, "fullName", "email", "passportNumber", "dateBirth", "specialNeeds");
+
+		super.getResponse().addData(dataset);
 	}
 
 }
