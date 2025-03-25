9694b51876d4bdaae24be2b198c353db1f028a18
DavidEscuderoAldana <davescald@alum.us.es>
2025-03-25 12:26:11 +0100

    Validador technician

--- src/main/java/acme/realms/Technician.java
+++ src/main/java/acme/realms/Technician.java
@@ -11,12 +11,15 @@ import acme.client.components.validation.Mandatory;
 import acme.client.components.validation.Optional;
 import acme.client.components.validation.ValidNumber;
 import acme.client.components.validation.ValidString;
+import acme.constraints.ValidLicenseNumber;
+import acme.constraints.ValidTechnician;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
+@ValidTechnician
 public class Technician extends AbstractRole {
 
 	// Serializaition version ----------------------------------------------
@@ -24,7 +27,7 @@ public class Technician extends AbstractRole {
 
 	// Attributes -------------------------------------------------------------
 	@Mandatory
-	@ValidString(pattern = "^[A-Z]{2,3}\\d{6}$")
+	@ValidLicenseNumber
 	@Column(unique = true)
 	private String				licenseNumber;
 
