38fe6adb04c5ceece4b41694a3ee71721f910e22
Adrián Robles Borrego <adrrobbor@alum.us.es>
2025-03-25 10:50:12 +0100

    Correccion parametros minimos y maximos

--- src/main/java/acme/realms/FlightCrewMembers.java
+++ src/main/java/acme/realms/FlightCrewMembers.java
@@ -31,7 +31,7 @@ public class FlightCrewMembers extends AbstractRole {
 	// Attributes -------------------------------------------------------------
 
 	@Mandatory
-	@ValidString(min = 8, max = 9, pattern = "^[A-Z]{2,3}\\d{6}$")
+	@ValidString(pattern = "^[A-Z]{2,3}\\d{6}$")
 	@Column(unique = true)
 	private String				employeeCode;
 
