600f4ca4f95df27bc0856e24b776b827904da330
naredo <nachonaredobernardos@gmail.com>
2025-04-01 09:02:44 +0200

    airlines

---
+++ src/main/java/acme/features/airline/AdministratorAirlineController.java
@@ -0,0 +1,24 @@
+
+package acme.features.airline;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import acme.client.components.principals.Administrator;
+import acme.client.controllers.AbstractGuiController;
+import acme.client.controllers.GuiController;
+import acme.entities.airline.Airline;
+
+@GuiController
+public class AdministratorAirlineController extends AbstractGuiController<Administrator, Airline> {
+
+	@Autowired
+	private AdministratorAirlineListService listService;
+
+
+	@PostConstruct
+	protected void initialise() {
+		super.addBasicCommand("list", this.listService);
+	}
+}
